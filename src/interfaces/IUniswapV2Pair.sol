23f63a52eb0393cb1703dc6dcb26813d4f3151d2
Albert <iavl@proton.me>
2025-09-15 20:13:56 +0800

    feat: update interface

--- src/interfaces/IUniswapV2Pair.sol
+++ src/interfaces/IUniswapV2Pair.sol
@@ -38,6 +38,7 @@ interface IUniswapV2Pair {
     function token0() external view returns (address);
     function token1() external view returns (address);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function feeBps() external view returns (uint bps);
     function price0CumulativeLast() external view returns (uint);
     function price1CumulativeLast() external view returns (uint);
     function kLast() external view returns (uint);
